b8cbd60400daecbc5cd016eb56bb786bdd052bf1
XingyuXiong <Peter__xiong@163.com>
2019-05-08 00:15:39 +0800

    add trainset and testset

--- main.py
+++ main.py
@@ -8,10 +8,16 @@ import sys, os
 import pickle
 
 TRAIN_NUM=1000
+ENABLE_RATE=False
+TRAIN_NUM_RATE=0.5
 MODEL_PATH = './model/hmm'
 
 def trainModel() :
-    dataSet = brown.tagged_words(tagset='universal')[:TRAIN_NUM]
+    if ENABLE_RATE:
+        end=TRAIN_NUM*TRAIN_NUM_RATE
+    else:
+        end=TRAIN_NUM
+    dataSet = brown.tagged_words(tagset='universal')[:end]
     dataSet = [ [d[0].lower(), d[1]] for d in dataSet ]
     hmm = HMM(dataSet)
     paras = hmm.output_to_viterbi()
@@ -43,6 +49,12 @@ Usage: ./main.py [size_of_training_set] sentence_to_predict')
         exit()
     elif len(sys.argv) == 2 and os.path.exists(MODEL_PATH):
         paras = loadModel()
+    elif len(sys.argv) == 2 and sys.argv[1] == '-t' :
+        ENABLE_RATE=True
+        paras = trainModel()
+        input_words=[tuple[0] for tuple in brown.tagged_words(tagset='universal')[TRAIN_NUM*TRAIN_NUM_RATE:TRAIN_NUM]]
+        sentence=' '.join(input_words)
+        predictCategory(paras, )
     else :
         TRAIN_NUM=int(sys.argv[1])
         paras = trainModel()
